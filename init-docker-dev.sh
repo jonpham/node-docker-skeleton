f7edf30d4fa7f518e1804e2fec54c5ffac2e1705
Jonathan Pham <jonathan@paysa.com>
2018-12-15 14:36:20 -0800

    Add init-docker-dev that should be sourced.

---
+++ init-docker-dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# SOURCE THIS FILE
+
+IMAGE_NAME='node-docker-skeleton'
+GROUP_NAME='jpham247'
+DEVELOPMENT_PORT=8080
+
+function build-docker {
+  docker build -t $GROUP_NAME/$IMAGE_NAME .
+}
+
+function run-docker {
+  docker run -p $DEVELOPMENT_PORT:80 $GROUP_NAME/$IMAGE_NAME
+}
